39ed45834a179c26fbd042ad642662e97bc4721b
Kristofer Liljeblad <kristol@microsoft.com>
2021-11-19 15:24:08 +0100

    initial commit

---
+++ OpenTKTest/Game.cs
@@ -0,0 +1,27 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKTest
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            var input = KeyboardState;
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Environment.Exit(0);
+            }
+
+            base.OnUpdateFrame(args);
+        }
+    }
+}
---
+++ OpenTKTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenTK.Windowing.Desktop;
+using OpenTKTest;
+
+Console.WriteLine("Hello, World!");
+
+
+var gameWindowSettings = new GameWindowSettings();
+var nativeWindowSettings = new NativeWindowSettings();
+
+using var game = new Game(gameWindowSettings, nativeWindowSettings);
+
+//Run takes a double, which is how many frames per second it should strive to reach.
+//You can leave that out and it'll just update as fast as the hardware will allow it.
+game.Run();
